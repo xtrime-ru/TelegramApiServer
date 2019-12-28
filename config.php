de22e60d79a4c0efa2e69575e672f642260e1e10
Alexander Pankratov <s.pankratoff@gmail.com>
2019-12-29 01:23:46 +0300

    Composer update & disable old updates handle

--- config.php
+++ config.php
@@ -18,6 +18,10 @@ return [
             'logger' => \danog\MadelineProto\Logger::ECHO_LOGGER, //  0 - Logs disabled, 3 - echo logs.
             'logger_level' => getenv('LOGGER_LEVEL'), // Logging level, available logging levels are: ULTRA_VERBOSE - 5, VERBOSE - 4 , NOTICE - 3, WARNING - 2, ERROR - 1, FATAL_ERROR - 0.
         ],
+        'updates' => [
+            'handle_updates' => true, // Should I handle updates?
+            'handle_old_updates' => false, // Should I handle old updates on startup?
+        ],
         'connection_settings' => [
             'all' => [
                 'proxy' => '\SocksProxy',
