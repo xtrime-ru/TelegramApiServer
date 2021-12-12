237415ec142f9e48385a06d88ffc863387639243
Alexander Pankratov <s.pankratoff@gmail.com>
2021-12-12 03:00:00 +0300

    Feat: pass args to build script

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-docker buildx build --platform linux/amd64,linux/arm64 -t xtrime/telegram-api-server:latest --push "$SCRIPT_DIR/../"
+docker buildx build --platform linux/amd64,linux/arm64 -t xtrime/telegram-api-server:latest "$@" --push "$SCRIPT_DIR/../"
