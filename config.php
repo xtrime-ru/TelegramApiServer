ca1504afbc70cb1c17de28c6cf4d5db452b72581
Alexander Pankratov <s.pankratoff@gmail.com>
2023-11-19 19:04:07 +0100

    Reduce floodwait timeout

--- config.php
+++ config.php
@@ -19,7 +19,7 @@ $settings = [
             'level' => (int)getenv('LOGGER_LEVEL'), // Logging level, available logging levels are: ULTRA_VERBOSE - 5, VERBOSE - 4 , NOTICE - 3, WARNING - 2, ERROR - 1, FATAL_ERROR - 0.
         ],
         'rpc' => [
-            'flood_timeout' => 10,
+            'flood_timeout' => 5,
             'rpc_drop_timeout' => 11,
         ],
         'connection' => [
