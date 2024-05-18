1e5d09c7d962fd81d4365a719fd118af95dd807d
Alexander Pankratov <s.pankratoff@gmail.com>
2024-05-18 18:36:19 +0200

    Fix: media download

--- config.php
+++ config.php
@@ -21,7 +21,7 @@ $settings = [
         ],
         'rpc' => [
             'flood_timeout' => 5,
-            'rpc_drop_timeout' => 11,
+            'rpc_drop_timeout' => 20,
         ],
         'connection' => [
             'max_media_socket_count' => 10,
@@ -56,6 +56,7 @@ $settings = [
         ],
         'files' => [
             'report_broken_media' => false,
+            'download_parallel_chunks' => 20,
         ],
     ],
     'api' => [
