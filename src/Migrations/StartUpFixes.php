ba8a8730a505e0e868c94ba1f572acde5cbc829e
Daniil Gentili <daniil@daniil.it>
2024-06-26 22:30:15 +0200

    Bump

--- src/Migrations/StartUpFixes.php
+++ src/Migrations/StartUpFixes.php
@@ -2,10 +2,13 @@
 
 namespace TelegramApiServer\Migrations;
 
+use danog\MadelineProto\Magic;
+
 class StartUpFixes
 {
     public static function fix(): void
     {
         define('MADELINE_WORKER_TYPE', 'madeline-ipc');
+        Magic::$isIpcWorker = true;
     }
 }
